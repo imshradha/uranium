706aa6c49d9a16560cf011788a3c82ab5ababa0a
imshradha <shraddha824swan@gmail.com>
2022-04-06 20:49:09 +0530

    adding remaining files

---
+++ src/routes/logger.js
@@ -0,0 +1,9 @@
+let endpoint =  "https://www.google.com"
+
+let log = function() {
+    console.log("I am inside the function")
+
+}
+
+module.exports.url = endpoint
+module.exports.logging = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
+const logger = require('./logger')
 
 router.get('/test-me', function (req, res) {
+    console.log("I am In");
+    console.log('The endpoint value is',logger.url)
+    console.log('calling log fuction')
+    logger.logging()
+    
     res.send('My first ever api!')
+});
+
+router.get('/test-me1', function (req, res) {
+    console.log("I am In");
+    res.send('My second ever api!')
 });
 
 module.exports = router;
