88dd553be7a33f5efae029c8d4328e673198c11a
imshradha <shraddha824swan@gmail.com>
2022-04-11 14:04:02 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -63,8 +63,51 @@ router.post('/test-post2', function (req, res) {
 
 const randomController= require("../controllers/randomController.js")
 //write a post request to accept an element in post request body and add it to the given array and return the new array
-router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
+router.post('/test-post3', randomController.addToArray ); //HANDLER/
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
 
+router.post('/players', function (req, res) {
+    let newPlayerData = req.body
+    // console.log(newPlayerData)
+    for( let i in players){
+        if(players[i].name === newPlayerData.name){
+            res.send( { msg : "Player already exists"})
+        }   
+    }
+            players.push(newPlayerData)
+            res.send(players)
+            //console.log(players)
+             
+});
 
 module.exports = router;
